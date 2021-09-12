cc537aa469d5242f87f1f19e85300b94a5dda2e6
DanielElias12 <daniel_alejan123@hotmail.com>
2021-09-11 20:35:56 -0600

    decodificacion

--- ConsolaLab2/Program.cs
+++ ConsolaLab2/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using LibreriaRD3;
-using static LibreriaRD3.Huffmann;
+//using static LibreriaRD3.Huffmann;
 
 namespace ConsolaLab2
 {
@@ -9,29 +9,44 @@ namespace ConsolaLab2
     {
         static void Main(string[] args)
         {
-            string Example = "ddabdccedchafbadgdcgabgccddbcdgg";
+            string Example = "Pero recuerda(nadie es perfecto y tú lo verás) (Más de mil cosas mejores tendrás, pero cariño sincero jamás)Vete olvidando, de esto que hoy dejas y que cambiarás Por la aventura que tú ya verás Será tu cárcel y nunca saldrás";
             var huffman = new Huffman<char>(Example);
+            
             List<int> encoding = huffman.Encode(Example);
-            List<char> decoding = huffman.Decode(encoding);
+            int largo = encoding.Count;
+            List<int> decodeascii = huffman.decodeascii(huffman.Encodeascii(encoding), largo);
+            
+            List<char> decoding = huffman.Decode(decodeascii);
+            string linea = "";
             var outString = new string(decoding.ToArray());
-            Console.WriteLine(outString == Example ? "Encoding/decoding worked" : "Encoding/Decoding failed");
+             for (int i=0; decoding.Count >i; i++)
+            {
+                linea += decoding[i];
 
+            }
+             
+            Console.WriteLine(outString == Example ? "Encoding/decoding worked" : "Encoding/Decoding failed");
+            Console.WriteLine(linea);
             var chars = new HashSet<char>(Example);
             foreach (char c in chars)
             {
                 encoding = huffman.Encode(c);
                 decoding = huffman.Decode(encoding);
                 Console.Write("{0}:  ", c);
+              
                 foreach (int bit in encoding)
                 {
                     Console.Write("{0}", bit);
                 }
+              
                 Console.WriteLine();
             }
 
-          
+
+
+            Console.ReadKey(); ;
             
-            Console.ReadKey();
         }
+        
     }
 }
--- LibreriaRD3/Huffman.cs
+++ LibreriaRD3/Huffman.cs
@@ -78,12 +78,118 @@ namespace LibreriaRD3
                 {
                     Encode(value, returnValue);
                 }
-                return returnValue;
+
+            //decodeascii(Encodeascii(returnValue));
+            
+            return returnValue;
             }
 
+        public List<string> Encodeascii(List<int> values)
+        {
+            while (values.Count % 8 != 0)
+            {
+                values.Add(0);
+
+            }
+            var returvalue = new List<string>();
+            //var bytearray = new List<int>();
+            string bytearray = ""; 
+
+            while (values != null)
+            {
+
+                for (int i = 0; i < 8; i++)
+                {
+                    if (values.Count > i)
+                    {
+                        bytearray += values[i];
+                    }
+                    //if (values.Count < 8)
+                    //{
+                    //   for (int  j = bytearray.Length; j < 8; j++)
+                    //    {
+                    //        bytearray += "0";
+
+                    //    }
+                    //}
+                }
+                if (values.Count > 8)
+                {
+                    for (int i = 0; i < 8; i++)
+                    {
+                        values.RemoveAt(0);
+                    }
+                }
+                else
+                {
+                    values = null;
+
+                }
+
+                var encEncoder = System.Text.Encoding.GetEncoding(28591);
+
+                string str = encEncoder.GetString(getbytesfrombinarystrin(bytearray));
+                bytearray = "";
+
+                returvalue.Add(str);
+                 
+            }
+      
+            return returvalue;
+        } 
+
+        public List<int> decodeascii(List<string> value, int largo)
+        {
+            //int i = 0;
+            string current = "";
+            byte[] bytearray;
+            var returvalue = new List<int>();
+            while (value != null)
+            {
+                current = value[0];
+                value.RemoveAt(0);
+                bytearray = System.Text.Encoding.GetEncoding(28591).GetBytes(current);
+               
+                for (int i = 0; i < bytearray.Length; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                       returvalue.Add((bytearray[i] & 0x80) > 0 ? 1 : 0);
+                        bytearray[i] <<= 1;
+                    }
+                }
+                current = "";
+
+                if (value.Count == 0)
+                {
+                    value = null;
+                }
+
+            }
+            var returvalue2 = new List<int>();
+            for (int i=0; i < largo; i++)
+            {
+
+                returvalue2.Add(returvalue[i]);
+
+
+            }
+            return returvalue2;
+        }
+
+        public Byte[] getbytesfrombinarystrin(string binary)
+        {
+            var list = new List<Byte>();
+         
+                list.Add(Convert.ToByte(binary, 2));
+         
+            return list.ToArray();
+        }
             public T Decode(List<int> bitString, ref int position)
             {
                 HuffmanNode<T> nodeCur = _root;
+            try
+            {
                 while (!nodeCur.IsLeaf)
                 {
                     if (position > bitString.Count)
@@ -92,10 +198,15 @@ namespace LibreriaRD3
                     }
                     nodeCur = bitString[position++] == 0 ? nodeCur.LeftChild : nodeCur.RightChild;
                 }
+            }
+            catch
+            {
+
+            }
                 return nodeCur.Value;
             }
 
-            public List<T> Decode(List<int> bitString)
+            public List<T> Decode(List<int> bitString) //este es  el que se llama
             {
                 int position = 0;
                 var returnValue = new List<T>();
