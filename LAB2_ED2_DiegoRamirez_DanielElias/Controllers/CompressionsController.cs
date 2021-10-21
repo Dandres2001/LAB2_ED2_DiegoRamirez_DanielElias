1969bc6b5d97ccd473dca89ac35113ba875770e6
Dandres2001 <dandres21nw@gmail.com>
2021-10-20 19:50:21 -0600

    detalles huffman

--- LAB2_ED2_DiegoRamirez_DanielElias/Controllers/CompressionsController.cs
+++ LAB2_ED2_DiegoRamirez_DanielElias/Controllers/CompressionsController.cs
@@ -22,7 +22,8 @@ namespace LAB2_ED2_DiegoRamirez_DanielElias.Controllers
     [ApiController]
     public class CompressionsController : ControllerBase
     {
-
+        static string ORIGINALFILENAME;
+        static string ORIGINALCONTENT;
         [HttpGet]
         public IEnumerable<string> Get()
         {
@@ -53,42 +54,32 @@ namespace LAB2_ED2_DiegoRamirez_DanielElias.Controllers
 
         // POST api/<CompressionsController>
         [HttpPost("compress/{name}")]
-        public FileResult Compress([FromRoute] string name,[FromForm] IFormFile File)
+        public async Task<FileResult> CompressAsync([FromRoute] string name,[FromForm] IFormFile File)
         {
-            
-            
-            var reader = new StreamReader(File.OpenReadStream());
-            string texto = reader.ReadToEnd();
-            reader.Close();
-            
-            //COMPRIMIENDO TEXTO
-            var huffman = new Huffman<char>(texto);
-            var keys = huffman.appearancesDictionary.Keys;
-            var values = huffman.appearancesDictionary.Values;
-            List<string> apariciones = new List<string>();
-            var encoder = Encoding.GetEncoding(28591);
-            string aux = "";
-            for (int i = 0; i < keys.Count(); i++)
-            {
-                apariciones.Add(keys.ElementAt(i).ToString());
-                aux = encoder.GetString(huffman.ConvertToByte(values.ElementAt(i).ToString()));
-                apariciones.Add(aux);
-            }
-            byte[] bytearray = apariciones.SelectMany(s=> Encoding.GetEncoding(28591).GetBytes(s)).ToArray();
-            List<int> encoding = huffman.Encode(texto);
-            string largo = encoder.GetString(huffman.ConvertToByte(encoding.Count.ToString()));
-            byte[] arrayLargo = Encoding.GetEncoding(28591).GetBytes(largo).ToArray();
-            List<string> compressed = huffman.Encodeascii(encoding);
-            byte[] dataAsBytes = compressed.SelectMany(s => Encoding.GetEncoding(28591).GetBytes(s)).ToArray();
 
+            var huffman = new Huffman();
+            byte[] fileData;
+            using (var memory = new MemoryStream())
+            {
+                await File.CopyToAsync(memory);
+
+
+                fileData = memory.ToArray();
+
+
+            }
+
+            //COMPRIMIENDO TEXTO
+            byte[] compressedData = huffman.Compress(fileData);
+          
             //ARCHIVANDO COMPRESION
             #region
             var newCompression = new CompModel();
             string NombreOriginal = File.FileName;
             double bytesOriginal = Convert.ToDouble(File.Length);
             string NombreComprimido = ".huff";
-            double factorDeCompresion = CalcularFactorCompresion(bytesOriginal, dataAsBytes.Length);
-            double razonDeCompresion = CalcularRazonCompresion(bytesOriginal, dataAsBytes.Length);
+            double factorDeCompresion = CalcularFactorCompresion(bytesOriginal, compressedData.Length);
+            double razonDeCompresion = CalcularRazonCompresion(bytesOriginal, compressedData.Length);
             string PorcentajeReducion = CalcularPorcentajeReducción(razonDeCompresion);
             var url = Request.Path;
             var requestUrl = $"{Request.Scheme}://{Request.Host.Value}/";
@@ -98,106 +89,171 @@ namespace LAB2_ED2_DiegoRamirez_DanielElias.Controllers
             newCompression.FactorCompresion = factorDeCompresion;
             newCompression.RazonCompresion = razonDeCompresion;
             newCompression.PorcentajeReduccion = PorcentajeReducion;
-
+            ORIGINALFILENAME = File.FileName;
+            ORIGINALCONTENT = File.ContentType;
             Singleton.Instance.CompList.Add(newCompression);
             #endregion
 
             //ESCRIBIENDO ARCHIVO
-            string text = bytearray + "\n" + dataAsBytes;
-            byte[] nullarray = { default };
+            
 
 
-            return base.File(Combine(arrayLargo, nullarray, bytearray,nullarray,dataAsBytes), "compressedFile / huff", name + ".huff");
+            return base.File(compressedData, "compressedFile / huff", name + ".huff");
         }
 
         // PUT api/<CompressionsController>/5
         [HttpPost("decompress")]
         public async Task<FileResult> Decompress([FromForm] IFormFile File)
         {
-          
-           
-            byte[] bytes;
+
+            var huffman = new Huffman();
+            byte[] fileData;
             using (var memory = new MemoryStream())
             {
                 await File.CopyToAsync(memory);
                 
                 
-                bytes = memory.ToArray();
-                List<byte> aux = bytes.OfType<byte>().ToList();
-
+                fileData = memory.ToArray();
+              
             }
-            int i = 0;
-            byte[] byteTamaño = new byte[bytes.Length];
-            byte[] apariciones = new byte[bytes.Length];
-            byte[] mensaje = new byte[bytes.Length];
-            int j = 0;
-            while (bytes[i] != 0)
-            {
-                
-                byteTamaño[j] = bytes[i];
-                i++;
-                j++;
-            }
-            i++;
-            j = 0;
-            while (bytes[i] != 0)
-            {
-                
-                apariciones[j] = bytes[i];
-                i++;
-                j++;
-            }
-            i++;
-            j = 0;
-            while (i != bytes.Length)
-            {
-               
-                mensaje[j] = bytes[i];
-                i++;
-                j++;
-            }
+            byte[] decompressedData = huffman.Decompress(fileData);
 
 
-            List<string> asciivalues = new List<string>();
-
-            var encoder = Encoding.GetEncoding(28591);
-            string currentletter = "";
-            string  mensajeordenado =  message(apariciones);
-            var huffman = new Huffman<char>(mensajeordenado);
-            for (int k = 0; k< mensaje.Length; k++)
-            {
-                currentletter = mensaje[k].ToString();
-               currentletter = encoder.GetString(ConvertToByte(currentletter));
-                asciivalues.Add(currentletter);
-                
-                if (mensaje[k] == 0)
-                {
-                    break;
-                }
-            }
-
-            List<int> decodeascii = huffman.decodeascii(asciivalues,byteTamaño[0]);
-            List<char> decoding = huffman.Decode(decodeascii);
             var lista = Singleton.Instance.CompList;
-            var stringFinal = new string(decoding.ToArray());
-            byte[] bytesFinales = Encoding.GetEncoding(28591).GetBytes(stringFinal);
-            string NombreOriginal;
 
-            if (lista.Count == 0)
-            {
-                NombreOriginal = "default";
-            }
-            else
-            {
-                NombreOriginal = lista.ElementAt(lista.Count - 1).NombreOriginal;
-            }
-
-        
-    
            
-            return base.File(bytesFinales,"text/plain", NombreOriginal);
+            return base.File(decompressedData, ORIGINALCONTENT, ORIGINALFILENAME);
         }
-        
+
+
+//        [HttpPost("lzw/compress/{name}")]
+//        public FileResult LZWCompress([FromRoute] string name, [FromForm] IFormFile File)
+//        {
+//            var compressor = new LZW<string>();
+//            var reader = new StreamReader(File.OpenReadStream());
+//            string texto = reader.ReadToEnd();
+//            reader.Close();
+//            byte[] archivoComprimido = compressor.Compress(texto);
+
+//            //GUARDANDO COMPRESIÓN
+//            var newCompression = new CompModel();
+//            string NombreOriginal = File.FileName;
+//            double bytesOriginal = Convert.ToDouble(File.Length);
+//            string NombreComprimido = ".lzw";
+//            double factorDeCompresion = CalcularFactorCompresion(bytesOriginal, archivoComprimido.Length);
+//            double razonDeCompresion = CalcularRazonCompresion(bytesOriginal, archivoComprimido.Length);
+//            string PorcentajeReducion = CalcularPorcentajeReducción(razonDeCompresion);
+//            var url = Request.Path;
+//            var requestUrl = $"{Request.Scheme}://{Request.Host.Value}/";
+//            string rutaYnombre = requestUrl + url + NombreComprimido;
+//            newCompression.NombreOriginal = NombreOriginal;
+//            newCompression.NombreRutadeArchivo = rutaYnombre;
+//            newCompression.FactorCompresion = factorDeCompresion;
+//            newCompression.RazonCompresion = razonDeCompresion;
+//            newCompression.PorcentajeReduccion = PorcentajeReducion;
+
+//            return base.File(archivoComprimido, "compressedFile / lzw", name + ".lzw");
+//        }
+
+//        [HttpPost("lzw/decompress")]
+//        public async Task<FileResult> LZWDecompress([FromForm] IFormFile File)
+//        {
+//            var compressor = new LZW<string>();
+//            byte[] bytes;
+//            using (var memory = new MemoryStream())
+//            {
+//                await File.CopyToAsync(memory);
+
+
+//                bytes = memory.ToArray();
+//                List<byte> aux = bytes.OfType<byte>().ToList();
+
+//            }
+//            int i = 0;
+       
+//            var maxBits = new List<Byte>();
+//            var largodiccionario = new List<Byte>();
+//            var repeticiones = new List<Byte>();
+//            var chardiccionario = new List<Byte>();
+//            var mensajecomprimido= new List<Byte>();
+//            var encoder = Encoding.GetEncoding(28591);
+//            var lista = Singleton.Instance.CompList;
+           
+//            while (bytes[i] != 0)
+//            {
+
+//                maxBits.Add(bytes[i]);
+//                i++;
+             
+//            }
+//            i++;
+//            string max = encoder.GetString(maxBits.ToArray());
+//            int bitsmax = 0;
+//            foreach (char c in max)
+//            {
+//                bitsmax = bitsmax + c;
+//            }
+//            while (bytes[i] != 0)
+//            {
+//                repeticiones.Add(bytes[i]);
+//                i++;
+//            }
+//            string repe = encoder.GetString(repeticiones.ToArray());
+//            int repetir = 0;
+//            foreach (char c in repe)
+//            {
+//                repetir = repetir + c;
+//            }
+//            i++;
+//            while (bytes[i] != 0)
+//            {
+
+//                 largodiccionario.Add(bytes[i]);
+//                i++;
+
+//            }
+//            i++;
+//            string tamañoDiccionario = encoder.GetString(largodiccionario.ToArray());
+//            int largo = 0;
+//            foreach (char c in tamañoDiccionario)
+//            {
+//                largo = largo + c;
+//            }
+//            int z = 0;
+//            while (largo !=z )
+//            {
+               
+//                 chardiccionario.Add( bytes[i]);
+//                z++;
+//                i++;
+                
+//            }
+
+//            while(i != bytes.Length)
+//            {
+//                mensajecomprimido.Add(bytes[i]);
+//                i++;
+//            }
+
+//            string NombreOriginal;
+//            if (lista.Count == 0)
+//            {
+//                NombreOriginal = "default";
+//            }
+//            else
+//            {
+//                NombreOriginal = lista.ElementAt(lista.Count - 1).NombreOriginal;
+//            }
+
+
+//            string mensajcomprimido =  compressor.Decompress(bitsmax, repetir, chardiccionario, mensajecomprimido);
+//            byte[] bytesFinales = encoder.GetBytes(mensajcomprimido);
+//            return base.File(bytesFinales, "text/plain", NombreOriginal+ ".txt");
+
+         
+//        }
+
+//>>>>>>> Stashed changes
         public string  message(byte[] aparaciones)
         {
             string mensaje = "";
